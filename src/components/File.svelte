7c7b13ffb9ffc08a658f634259ab99e9531c1dc3
johannchopin <johannchopin@pm.me>
2021-02-18 17:15:26 +0100

    ✨ Terminale component

--- src/components/File.svelte
+++ src/components/File.svelte
@@ -1,9 +1,10 @@
 <script>
-	export let name
-  export let content
   import { onMount } from "svelte"
   import { initHighlightJs } from "../utils"
+  import Terminal from './Terminal.svelte'
 
+	export let name
+  export let content
   export let expended = false
 
   const toggle = () => {
@@ -22,37 +23,10 @@
 		background-size: 1em 1em;
 		cursor: pointer;
 	}
-
-  pre {
-    max-width: 300px;
-
-    code {
-      display: inline-block;
-    }
-  }
-  
-  .editor {
-    width: fit-content;
-    overflow: hidden;
-
-    .editor-toolbar {
-      div {
-        height: 12px;
-        width: 12px;
-      }
-    }
-  }
 </style>
 
 <span on:click={toggle} class="ps-0">📜 {name}</span>
 
-<div class="editor d-flex flex-column ms-4 mt-2 rounded-3" class:d-none={!expended}>
-  <div class="editor-toolbar d-flex ps-2 py-1 bg-light">
-    <div class="rounded-circle mx-1 bg-danger"> </div>
-    <div class="rounded-circle mx-1 bg-warning"> </div>
-    <div class="rounded-circle mx-1 bg-success"> </div>
-  </div>
-  <pre>
-    <code class="json rounded-3 p-3">{JSON.stringify(content, null, 2)}</code>
-  </pre>
+<div class:d-none={!expended}>
+  <Terminal content={JSON.stringify(content, null, content[0] !== null ? 2 : 0)} language="json" />
 </div>
---
+++ src/components/Terminal.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let content: string
+  export let language: string = ''
+</script>
+
+<style lang="scss">
+  pre {
+    width: 300px;
+
+    code {
+      display: inline-block;
+      width: 100%;
+    }
+  }
+
+  .editor {
+    width: fit-content;
+    overflow: hidden;
+
+    .editor-toolbar {
+      div {
+        height: 12px;
+        width: 12px;
+      }
+    }
+  }
+</style>
+
+<div class="editor d-flex flex-column ms-4 mt-2 rounded-3">
+  <div class="editor-toolbar d-flex ps-2 py-1 bg-light">
+    <div class="rounded-circle mx-1 bg-danger"> </div>
+    <div class="rounded-circle mx-1 bg-warning"> </div>
+    <div class="rounded-circle mx-1 bg-success"> </div>
+  </div>
+  <pre>
+    <code class={`${language} rounded-3 p-3`}>{content}</code>
+  </pre>
+</div>
