5509f18d5b0e065ad1e7a3239f32c42288409309
johannchopin <johannchopin@pm.me>
2021-03-07 09:51:00 +0100

    💄 Fix examples layout

--- src/routes/examples.svelte
+++ src/routes/examples.svelte
@@ -3,7 +3,7 @@
     {
       link: 'https://github.com/johannchopin/restapify-examples/tree/main/github-api',
       title: 'GitHub API',
-      description: 'Proof of concept that you can mock a real API architecture by mocking some enpoints of the <a href="https://docs.github.com/en/enterprise-server@2.22/rest/reference">GitHub API</a>.'
+      description: 'Proof of concept that you can mock a real API architecture by mocking some endpoints of the <a href="https://docs.github.com/en/enterprise-server@2.22/rest/reference">GitHub API</a>.'
     },
     {
       link: 'https://github.com/johannchopin/restapify-examples/tree/main/nextjs-animal-shelter-app',
@@ -24,12 +24,12 @@
   <p>Explore some <a href="https://github.com/johannchopin/restapify-examples">examples</a> that describe the use of Restapify in real projects:</p>
 
   <div class="row mt-5">
-    <div class="col-md-8 w-100 d-md-flex justify-content-center">
+    <div class="col-md-8 w-100 d-flex flex-column flex-md-row justify-content-center align-items-center">
       {#each PROJECTS as { link, title, description}}
         <div class="card border-primary mx-3 mb-3" style="max-width: 18rem;">
           <div class="card-body text-primary">
             <h5 class="card-title">
-              <a href={link}>
+              <a href={link} target="_blank">
                 {title}
               </a>
             </h5>
