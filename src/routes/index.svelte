ed1992d978a7957a25735ead459043e5209c61e5
johannchopin <johannchopin@pm.me>
2021-03-07 19:49:04 +0100

    🔍 Add tiny description to home page title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -44,7 +44,7 @@
 </style>
 
 <svelte:head>
-	<title>Restapify</title>
+	<title>Restapify • Quickly and easily deploy a mocked REST API</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.6.0/styles/vs2015.min.css" integrity="sha512-w8aclkBlN3Ha08SMwFKXFJqhSUx2qlvTBFLLelF8sm4xQnlg64qmGB/A6pBIKy0W8Bo51yDMDtQiPLNRq1WMcQ==" crossorigin="anonymous" />
 </svelte:head>
 
