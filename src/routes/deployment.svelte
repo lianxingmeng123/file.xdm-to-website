59c017eb8b55556f7f2b03082c256ac6576a2c27
johannchopin <johannchopin@pm.me>
2021-08-15 16:38:10 +0200

    ✨ Add deployment page

---
+++ src/routes/deployment.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  import Documentation from '../components/Documentation.svelte'
+
+  export async function load({ fetch }) {
+    const response = await fetch('https://raw.githubusercontent.com/johannchopin/restapify/main/docs/deployment/README.md')
+    const inlineMd = await response.text()
+    
+		return { props: { inlineMd }}
+	}
+</script>
+
+<script>
+  export let inlineMd
+</script>
+
+<Documentation
+  pageSlug='/deployment'
+  pageTitle='Deployment • Restapify'
+  markdown={inlineMd} 
+  editUrl='https://github.com/johannchopin/restapify/edit/main/docs/deployment/README.md' 
+/>
